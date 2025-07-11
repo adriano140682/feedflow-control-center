6bf29d40e6e7cdd35ee6bc08b0de0af356086265
adriano140682 <adriano.prazeres@supremax.ind.br>
2025-07-10 22:47:32 -0300

    fix: ajusta configuração completa do Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDkb9hx58oPEynIkqKaKiflO1113IDzyuI",
   authDomain: "bdnovo-bb81f.firebaseapp.com",
   projectId: "bdnovo-bb81f",
-  storageBucket: "bdnovo-bb81f.firebasestorage.app",
+  storageBucket: "bdnovo-bb81f.appspot.com",
   messagingSenderId: "198891935516",
   appId: "1:198891935516:web:52c61286c6961b6a1f0cd9"
 };
 
-// Initialize Firebase
+// Inicializa o Firebase e o Firestore
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
